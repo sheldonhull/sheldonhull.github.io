8d9f2c194f6f96120edcb68550af2a3fc4e198f8
SheldonH <sheldonhull@gmail.com>
2018-01-19 22:42:50 -0600

    no message

--- RunInDocker.ps1
+++ RunInDocker.ps1
@@ -1,4 +1,4 @@
 #JEKYLL_ENV=development make it crash,
-docker run --volume="$PWD":/srv/jekyll --name sheldonhull.github.io `
+docker run --volume="$PWD":/srv/jekyll  `
 -it -p 127.0.0.1:4000:4000 -e JEKYLL_ENV=docker jekyll/jekyll:latest `
 jekyll s --config _config.yml,_config.dev.yml --force_polling --incremental
