68cc820cd0b6a23dd5a3635f1abe94d484a79590
SheldonH <sheldonhull@users.noreply.github.com>
2018-03-19 11:53:40 -0500

    build changes

--- serve.ps1
+++ serve.ps1
@@ -2,5 +2,5 @@
 set-location $PSScriptRoot -Verbose
 write-verbose (get-variable pwd | Ft -AutoSize -wrap | out-string)
 docker run --volume="$PWD":/srv/jekyll  `
--it -p 127.0.0.1:4000:4000 -e JEKYLL_ENV=docker jekyll/jekyll:latest `
+-it -p 127.0.0.1:4000:4000 -e JEKYLL_ENV=production jekyll/jekyll:latest `
 jekyll s --port 4000 --config _config.yml,_config.dev.yml --force_polling --incremental --trace
